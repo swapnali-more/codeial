5d53a57008559b4c24f368f4a9f1f0efbee44ad0
swapnali <swapnalim182@gmail.com>
2023-01-09 22:21:01 +0530

    manual authentication

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -2,9 +2,19 @@ const User = require('../models/user')
 
 // render the profile page
 module.exports.profile = function (req, res) {
-    return res.render('user_profile', {
-        title: 'Profile'
-    })
+    if(req.cookies.user_id) {
+        User.findById(req.cookies.user_id, function(err, user) {
+            if(user) {
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user: user
+                })
+            } 
+            return res.redirect('/users/sign-in')
+        })
+    } else {
+        return res.redirect('/users/sign-in')
+    }
 }
 
 // render the sign up page
@@ -47,5 +57,27 @@ module.exports.create = function(req, res) {
 
 //get the sign in data
 module.exports.createSession = function(req, res) {
-    
+    //step to authentication
+    //find the user
+    console.log(req.body, "login")
+    User.findOne({email: req.body.email}, function(err, user) {
+        
+        if(err) {
+            console.log('error in finding user in signing in!');
+            return;
+        }
+
+        //handle use found
+        if(user) {
+            //handle password which doesn't match
+            if(user.password != req.body.password) {
+                return res.redirect('back')
+            }
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        } else {
+            //handle user not found
+        }
+    })  
 }
